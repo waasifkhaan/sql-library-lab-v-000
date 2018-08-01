dce20179404e2fe7b8e72122da88b55844733b93
Wasif Khan <waasif.khaan@gmail.com>
2018-08-01 21:12:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON characters.series_id = series.id GROUP BY characters.species HAVING characters.species = 'human' ORDER BY COUNT(characters.species) desc LIMIT 1; "
+  "SELECT series.title FROM series INNER JOIN characters ON characters.series_id = series.id GROUP BY characters.species HAVING characters.species = 'human' ORDER BY COUNT(characters.species) asc LIMIT 1; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
